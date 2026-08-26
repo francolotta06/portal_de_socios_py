e8839ae7e5967a23701f0eb8bbbf271864f89eeb
Franco <francolotta06@gmail.com>
2026-08-25 22:44:26 -0300

    desplegar en render actualizaciones

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'clave-secreta-para-las-sesiones-2026*')
-    MONGO_URI = 'mongodb://localhost:27017/'
-
+    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/portal_socios')
     # PayPal Sandbox
     # Obtene tus credenciales en: https://developer.paypal.com/dashboard/applications/sandbox
     PAYPAL_CLIENT_ID = os.environ.get('PAYPAL_CLIENT_ID', 'Abh0JxxPcISes03SpcFPUVPdEqebctbVuU_rBW8jjx10i6Ifc9UPOcxFKhzgRUeHd2Rsgm0V6i1IzgCv')
